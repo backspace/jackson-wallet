505e62a482071ad44814af930a93b649917b713f
Buck Doyle <b@chromatin.ca>
2022-09-23 17:33:46 -0500

    Add balance display for hardcoded wallet

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import { ethers } from "ethers";
+import { onMounted, ref } from "vue";
+
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://goerli-rollup.arbitrum.io/rpc"
+);
+const walletAddress = "0x30c3fDF579F8A0DAbbeE9AEFdaf64AAa39113260";
+
+const loaded = ref(false);
+const balanceString = ref("");
+
+onMounted(() => {
+  provider.getBalance(walletAddress).then((v) => {
+    balanceString.value = ethers.utils.formatEther(v);
+    loaded.value = true;
+  });
+});
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div v-if="loaded">
+      Balance for wallet {{ walletAddress }}: {{ balanceString }} ETH
+    </div>
+    <div v-if="!loaded">…</div>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
